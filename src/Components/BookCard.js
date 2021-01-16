6d2bc65aeb6897dc0c2c6c3c0e14a4bc69d1b8e8
chrisvsanchez <chrisvsanchez@gmail.com>
2021-01-16 15:21:36 -0500

    first commit

---
+++ src/Components/BookCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookCard = () => {
+  return <></>;
+};
+
+export default BookCard;
---
+++ src/Components/Search.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <>
+      <form
+      //   onSubmit={}
+      >
+        <input type="text" value="" name="searchTerm" />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Search;
