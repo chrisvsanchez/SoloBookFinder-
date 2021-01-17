18531ed25b5328ae0b11ffe3f2cef424be11fe5b
chrisvsanchez <chrisvsanchez@gmail.com>
2021-01-17 14:55:20 -0500

    updated api name in .env file to REACT_APP_GOOGLE_BOOKS_API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import Search from "./Components/Search";
+const API_KEY = process.env.REACT_APP_GOOGLE_BOOKS_API;
+console.log(process.env.REACT_APP_GOOGLE_BOOKS_API);
 function App() {
-  const API_KEY = process.env.GOOGLE_BOOKS_API;
   // const API_ENDPOINT = "";
   const [searchTerm, setSearchTerm] = useState("");
   const handleSearchInput = (e) => {
     setSearchTerm(e.target.value);
   };
-  // useEffect(() => {
-  //   fetch().then;
-  // }, [searchTerm]);
+  useEffect(() => {
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${searchTerm}:keyes&key=${API_KEY}`
+    )
+      .then((r) => r.json())
+      .then((books) => {
+        console.log(books);
+        // books.items.map(book =>{
+        //   book[0]
+        // })
+      });
+  }, [searchTerm]);
   return (
     <div className="main-container">
       <Search searchTerm={searchTerm} handleSearchInput={handleSearchInput} />
